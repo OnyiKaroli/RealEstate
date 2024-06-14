6c20da23b3e2eea64f8bc7474ac0652b253920da
OnyiKaroli <karolionyango@gmail.com>
2024-06-14 21:38:58 +0300

    Create a test API route

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -1,22 +1,25 @@
 import mongoose from "mongoose";
 import { type } from "os";
 
-const userSchema = new mongoose.Schema({
+const userSchema = new mongoose.Schema(
+  {
     username: {
-        type: String,
-        required: true,
-        unique: true,
+      type: String,
+      required: true,
+      unique: true,
     },
     email: {
-        type: String,
-        required: true,
-        unique: true,
+      type: String,
+      required: true,
+      unique: true,
     },
     password: {
-        type: String,
-        required: true,
-    }
-    }, { timestamps: true });
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
 
-    const User = mongoose.model("User", userSchema);
-    export default User;
+const User = mongoose.model("User", userSchema);
+export default User;
